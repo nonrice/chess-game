ab79ccf7357d48f1e2a66a0156eb4b3f1d2bf6b3
Eric Xue <widefin@gmail.com>
2022-07-29 14:36:47 -0400

    finish chess

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ import { Chess } from './chess.js'
 const GAME = new Chess();
 var SEL = false;
 var SEL_CELL = "";
+
+// NEED TO INITIALIZE THIS PER PLAYER
 var IS_WHITE = true;
 
 init_board(IS_WHITE);
@@ -12,18 +14,44 @@ function click(){
     if (!SEL){
         SEL_CELL = cell;
         SEL = true;
+        draw_moves(SEL_CELL);
     } else {
+        var promotion_piece = "";
+        if (can_promote(SEL_CELL)){
+            while (true){
+                promotion_piece = prompt("Piece to promote to? (n, b, r, k, q)");
+                if (promotion_piece.length == 1 && "nbrkq".includes(promotion_piece)){
+                    break;
+                }
+                alert("Not a valid piece dumbo");
+            }
+        }
+
+        // NEED TO SOMEHOW SEND THIS TO THE OTHER GUY
         GAME.move({
             from: SEL_CELL,
-            to: cell 
+            to: cell,
+            promotion: promotion_piece  
         });
+
         draw_pieces();
+        clear_moves();
         SEL = false;
     }
 
     if (GAME.game_over()){
         alert("GAME IS OVER")
     }
+}
+
+function can_promote(cell){
+    console.log(GAME.moves({square: cell, verbose: true}));
+    for (const move of GAME.moves({ square: cell, verbose: true })){
+        if (move.flags.includes('p')){
+            return true;
+        }
+    }
+    return false;
 }
 
 function draw_pieces(){
@@ -35,6 +63,25 @@ function draw_pieces(){
         } else {
             cell.innerHTML = "";
         }
+    }
+}
+
+function draw_moves(cell){
+    const moves = GAME.moves({
+        square: cell,
+        verbose: true
+    });
+
+    clear_moves();
+    document.getElementById(cell).classList.add("board-cell--selected");
+    for (const move of moves){
+        document.getElementById(move.to).classList.add("board-cell--selected");
+    }
+}
+
+function clear_moves(){
+    for (var r=0; r<8; ++r) for (var c=0; c<8; ++c) {
+        document.getElementById(get_cell(r, c)).classList.remove("board-cell--selected");
     }
 }
 
